8774444c350799aea7d6f29c56c4c0f5029313d6
Liz Love <love.elizabeth1989@gmail.com>
2020-06-01 15:25:46 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,7 @@ def find_min_in_nested_arrays(src)
  row_index = 0
  while row_index < src.count do
    element_index = 0
-   while element_index < src[row_index] do
+   while element_index < src[row_index].count do
      outer_results << src[row_index][element_index].min
      element_index += 1
    end
